3b9ade2e506815a5178b3f266e5db3773304b637
seppeverbeken <seppe.verbeken@student.ehb.be>
2023-11-22 14:11:45 +0100

    Replaced Git project with Local project
    After working on a local file for 2/3 weeks I am now replacing the project from github with my local files as they are the correct ones.

---
+++ build/images/api/src/index.js
@@ -0,0 +1,34 @@
+const { checkString } = require('./helpers/helper.js');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+
+const app = express();
+const port = 3000;
+
+// Knex.js configuration
+const knexConfig = require('../knexfile'); // Assuming knexfile.js is in the same directory
+
+// Initialize Knex.js with the configuration
+const db = knex(knexConfig.development);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// get routes from the routes folder
+const books = require('./routes/books.js');
+
+app.get('/', (request, response) => {
+  response.send('Hello World');
+});
+
+app.listen(port, (err) => {
+  if (!err) {
+    console.log('Server is running on port 3000');
+  } else {
+    console.log(err);
+  }
+});
